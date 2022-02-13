39fa79f3939461adf74bca197499fb6f8a4e722d
abraaowendel <abraao.dev@gmail.com>
2022-02-13 13:03:24 -0300

    projeto finalizado.

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">
+  <br>Calculadora
+</h1>
+
+<h4 align="center">
+  Calculadora com as operações.
+  Soma, Subtraí, Divide e Multiplica.
+</h4>
+
+<h4 align="center"><a href="https://abraaowendel.github.io/Calculadora/" target="_blank">Clique para visitar o projeto</a></h4>
+
+## 💼 Tecnologias utilizadas
+
+Para o desenvolvimento deste projeto usei as seguintes tecnologias:
+
+- HTML5;
+- CSS3;
+- JavaScript;
+
+---
+
+## ⚠️ Atenção!
+
+A função eval() não é recomendada para se usar em determinados projetos, pois possibilita usuários
+mal intencionados executarem comandos maliciosos através da mesma.
+
+---
---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function inserirNumero(num){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML += num
+}
+
+function calcular(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+    else{
+        alert('NADA FOI ADICIONADO A CALCULADORA')
+    }
+}
+
+function limparTudo(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = ''
+}
+
+function limpaUltimo(){
+   let resultado = document.getElementById('resultado').innerHTML;
+   document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+document.querySelectorAll('.buttons').
+forEach(b => b.addEventListener('click', () => inserirNumero(b.value)))
+document.querySelector('#calcular').addEventListener('click', calcular)
+document.querySelector('#limpaTudo').addEventListener('click', limparTudo)
+document.querySelector('#limpaUltimo').addEventListener('click', limpaUltimo)
+document.querySelector('#calcular').addEventListener('dblclick', limparTudo)
